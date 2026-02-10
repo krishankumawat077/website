0779cbf967b366a387164c78a919d641e9333b20
krishankumawat077 <krishan.kumawat077@gmail.com>
2026-02-10 22:59:42 +0530

    Frontend ready for Netlify deployment

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:5000";
+const API = "http://localhost:5000";  //Temp - backend not deployed yet
 
 document.getElementById("studentForm")?.addEventListener("submit", async (e) => {
   e.preventDefault();
